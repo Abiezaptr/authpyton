8230dc8dbc8cb3aecfb1386b8e325e43e1b3119a
Abiezaptr <abieza.eresha@gmail.com>
2024-01-08 11:40:44 +0700

    update sample auth

---
+++ config.py
@@ -0,0 +1,29 @@
+"""Configuration settings for running the Python auth samples locally.
+
+In a production deployment, this information should be saved in a database or
+other secure storage mechanism.
+"""
+
+CLIENT_ID = 'a10d6918-5758-4f0f-b716-787053507b52'
+CLIENT_SECRET = '7uU8Q~ekwzwEgyjYlvXFMuBFxIeNNFnxMKWG7bj_'
+REDIRECT_URI = 'http://localhost:5000/login/authorized'
+
+# AUTHORITY_URL ending determines type of account that can be authenticated:
+# /organizations = organizational accounts only
+# /consumers = MSAs only (Microsoft Accounts - Live.com, Hotmail.com, etc.)
+# /common = allow both types of accounts
+AUTHORITY_URL = 'https://login.microsoftonline.com/common'
+
+AUTH_ENDPOINT = '/oauth2/v2.0/authorize'
+TOKEN_ENDPOINT = '/oauth2/v2.0/token'
+
+RESOURCE = 'https://graph.microsoft.com/'
+API_VERSION = 'v1.0'
+SCOPES = ['User.Read'] # Add other scopes/permissions as needed.
+
+
+# This code can be removed after configuring CLIENT_ID and CLIENT_SECRET above.
+if 'ENTER_YOUR' in CLIENT_ID or 'ENTER_YOUR' in CLIENT_SECRET:
+    print('ERROR: config.py does not contain valid CLIENT_ID and CLIENT_SECRET')
+    import sys
+    sys.exit(1)
---
+++ sample_auth.py
@@ -0,0 +1,76 @@
+import os
+import urllib.parse
+import uuid
+from flask import Flask, redirect, request, render_template, jsonify
+import adal
+import requests
+
+app = Flask(__name__, template_folder='static/templates')
+app.debug = True
+app.secret_key = 'development'
+
+SESSION = requests.Session()
+
+@app.route('/')
+def homepage():
+    """Render halaman utama."""
+    return render_template('homepage.html', sample='ADAL')
+
+@app.route('/login')
+def login():
+    """Minta pengguna untuk mengotentikasi."""
+    auth_state = str(uuid.uuid4())
+    SESSION.auth_state = auth_state
+
+    # Untuk contoh ini, pengguna memilih akun untuk mengotentikasi.
+    prompt_behavior = 'select_account'
+
+    params = urllib.parse.urlencode({
+        'response_type': 'code',
+        'client_id': 'a10d6918-5758-4f0f-b716-787053507b52',  # Ganti dengan client ID Anda
+        'redirect_uri': 'http://localhost:5000/login/authorized',
+        'state': auth_state,
+        'resource': 'https://graph.microsoft.com/',  # Ganti dengan resource yang Anda inginkan
+        'prompt': prompt_behavior
+    })
+
+    return redirect('https://login.microsoftonline.com/common/oauth2/authorize?' + params)  # Ganti dengan authority URL Anda
+
+@app.route('/login/authorized')
+def authorized():
+    """Penangan untuk Redirect Uri aplikasi."""
+    code = request.args['code']
+    auth_state = request.args['state']
+
+    if auth_state != SESSION.auth_state:
+        raise Exception('state returned to redirect URL does not match!')
+
+    auth_context = adal.AuthenticationContext('https://login.microsoftonline.com/common', api_version=None)
+    token_response = auth_context.acquire_token_with_authorization_code(
+        code, 'http://localhost:5000/login/authorized', 'https://graph.microsoft.com/',
+        'a10d6918-5758-4f0f-b716-787053507b52', '7uU8Q~ekwzwEgyjYlvXFMuBFxIeNNFnxMKWG7bj_'  # Ganti sesuai kebutuhan
+    )
+
+    # Dapatkan data dari Microsoft Graph
+    graph_endpoint = 'https://graph.microsoft.com/v1.0/me'
+    http_headers = {'Authorization': f"Bearer {token_response['accessToken']}"}
+    graph_data = SESSION.get(graph_endpoint, headers=http_headers, stream=False).json()
+
+    # Redirect ke halaman Laravel sambil membawa semua data di URL
+    redirect_url = (
+        f"http://192.168.0.176/microsoft/callback?"
+        f"displayName={graph_data.get('displayName')}&"
+        f"givenName={graph_data.get('givenName')}&"
+        f"id={graph_data.get('id')}&"
+        f"jobTitle={graph_data.get('jobTitle')}&"
+        f"mail={graph_data.get('mail')}&"
+        f"mobilePhone={graph_data.get('mobilePhone')}&"
+        f"officeLocation={graph_data.get('officeLocation')}&"
+        f"preferredLanguage={graph_data.get('preferredLanguage')}&"
+        f"surname={graph_data.get('surname')}&"
+        f"userPrincipalName={graph_data.get('userPrincipalName')}"
+    )
+    return redirect(redirect_url)
+
+if __name__ == '__main__':
+    app.run(port=5000)
